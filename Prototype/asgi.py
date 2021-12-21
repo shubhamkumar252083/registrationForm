dea4cf4da916b802db953dfb446a1f1dfa501096
shubham <shubhamkumar252083@gmail.com>
2021-12-21 17:14:51 +0530

    setup for prototype

--- Prototype/asgi.py
+++ Prototype/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mainApp project.
+ASGI config for Prototype project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prototype.settings')
 
 application = get_asgi_application()
--- Prototype/wsgi.py
+++ Prototype/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mainApp project.
+WSGI config for Prototype project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prototype.settings')
 
 application = get_wsgi_application()
